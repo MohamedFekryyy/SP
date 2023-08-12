c21acc299bf0061f6936cbe0e4034c036f87c3d6
Fekry Studio <mfaiad2000@gmail.com>
2023-08-12 20:38:53 +0300

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -25,28 +25,28 @@ const handleProgress = (stepIncrement) => {
 </section>
 
 <style lang="postcss">
-    :global(html) {
-        background-color: theme(colors.slate.900);
-    }
-h1 {
-    @apply text-slate-50 text-4xl font-semibold mb-4;
-}
-h3 {
-    @apply text-slate-400 text-lg font-normal;
-}
-.section {
-    @apply min-w-full flex flex-col items-center justify-start  py-32 min-h-screen;
-}
+   :global(html) {
+		background-color: theme(colors.slate.950);
+	}
+	h1 {
+		@apply text-slate-50 text-5xl font-semibold mb-4 text-center;
+	}
+	h3 {
+		@apply text-slate-400 text-lg font-normal text-center;
+	}
+	.section {
+		@apply min-w-full flex flex-col items-center justify-start sm:justify-center  py-12 sm:py-32 min-h-screen px-5;
+	}
 
-.btn-next {
-    @apply bg-purple-300 px-6 py-3 text-lg font-medium rounded-lg text-blue-950;
-}
+	.btn-next {
+		@apply bg-purple-700 px-6 py-3 text-lg font-medium rounded-lg text-slate-50 hover:bg-purple-700/90  shadow-2xl shadow-purple-700/40  transition-all;
+	}
 
-.step-button {
-    @apply  max-w-[700px] mx-auto mt-10;
-}
+	.step-button {
+		@apply max-w-[700px] mx-auto  flex flex-col sm:flex-row justify-between  items-stretch;
+	}
 
-.btn-back {
-    
-}
+	.btn-back {
+		@apply bg-slate-900 min-h-full mr-3 rounded-lg px-5 hover:bg-slate-900/50 transition-all cursor-pointer items-center justify-center flex;
+	}
 </style>
