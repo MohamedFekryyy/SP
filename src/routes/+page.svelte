182a087b88bb9039c4e96be0d6747020ab4289aa
Fekry Studio <mfaiad2000@gmail.com>
2023-08-13 18:40:58 +0300

    Added cool button

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -189,9 +189,14 @@ function handleClosePrivacyModal() {
         {/if}
 
         {#if currentActive === steps.length}
-        <button class="btn-next w-auto" on:click={() => generatePDF(formData, items, milestones, startDate, endDate, totalSum)} disabled={currentActive != steps.length}
-            >
-            Generate Proposal
+      
+        <button on:click={() => generatePDF(formData, items, milestones, startDate, endDate, totalSum)} disabled={currentActive != steps.length} class="relative inline-flex items-center justify-center inline-block p-4 px-6 py-3 overflow-hidden font-medium text-indigo-600 rounded-lg shadow-2xl group">
+            <span class="absolute top-0 left-0 w-60 h-60 -mt-10 -ml-3 transition-all duration-700 bg-purple-700 rounded-full blur-md ease"></span>
+            <span class="absolute inset-0 w-full h-full transition duration-700 group-hover:rotate-180 ease">
+            <span class="absolute bottom-0 left-0 w-24 h-24 -ml-10 bg-pink-700 rounded-full blur-md"></span>
+            <span class="absolute bottom-0 right-0 w-24 h-24 -mr-10 bg-blue-900 rounded-full blur-xl"></span>
+            </span>
+            <span class="relative text-xl text-white">Generate Proposal</span>
         </button>
         
           
