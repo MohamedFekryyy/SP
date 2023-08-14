c812ec8bdbdc53c90605b0aadacb5e1c0a4bb4f0
Fekry Studio <mfaiad2000@gmail.com>
2023-08-14 19:18:35 +0300

    UI updates & installed puppeteer

--- src/components/Form.svelte
+++ src/components/Form.svelte
@@ -68,15 +68,15 @@
 </script>
 
 
-<form class="form-container bg-slate-900/50 backdrop-blur-xl border border-slate-900 rounded-2xl px-4 sm:px-6 pt-4 sm:pt-6 pb-none sm:pb-1 max-w-[800px] my-10 mx-auto shadow-2xl shadow-slate-950 transition-all" on:submit={handleSubmit}>
+<form class="form-container bg-slate-900/50 backdrop-blur-xl border border-slate-900 rounded-2xl px-4 sm:px-6 pt-4 sm:pt-6 pb-none sm:pb-1 max-w-[800px] my-6 sm:my-10 mx-auto shadow-2xl shadow-slate-950 transition-all" on:submit={handleSubmit}>
 	{#if active_step == 'clientInfo'}
 	<h2 in:fade={{ duration: 400 }} out:slide={{ y: -30, duration: 250 }} class="sm:text-2xl text-xl">Client Info</h2>
-	<div in:fade={{ duration: 400 }} out:slide={{ y: -30, duration: 250 }} class="flex flex-col sm:flex-row gap-4"><div class="w-full sm:w-[50%]"><InputField label={'Contact Person'} bind:value={formData.clientName} placeholder="Elon Musk"/></div>
+	<div in:fade={{ duration: 400 }} out:slide={{ y: -30, duration: 250 }} class="flex flex-col sm:flex-row gap-x-4"><div class="w-full sm:w-[50%]"><InputField label={'Contact Person'} bind:value={formData.clientName} placeholder="Elon Musk"/></div>
 	<div class="w-full sm:w-[50%]"><InputField label={'Company Name'} bind:value={formData.clientCompany} placeholder="X"/></div></div>
 		<div in:fade={{ duration: 400, delay: 50 }} out:slide={{ y: -30, duration: 250 }}><InputField type={'email'} label={'Client Email'} bind:value={formData.clientEmail} placeholder="elon@x.com"/></div>
 		<h2 in:fade={{ duration: 400, delay: 200 }} out:slide={{ y: -30, duration: 250 }} class="mt-4 sm:mt-8 sm:text-2xl text-xl">Your Info</h2>
 
-		<div in:fade={{ duration: 400 }} out:slide={{ y: -30, duration: 250 }} class="flex flex-col sm:flex-row gap-4"><div in:fade={{ duration: 400, delay: 100 }} out:slide={{ y: -30, duration: 250 }} class="w-full sm:w-[50%]"><InputField label={'Your Name'} bind:value={formData.freelancerName} placeholder="John Doe"/></div>
+		<div in:fade={{ duration: 400 }} out:slide={{ y: -30, duration: 250 }} class="flex flex-col sm:flex-row gap-x-4"><div in:fade={{ duration: 400, delay: 100 }} out:slide={{ y: -30, duration: 250 }} class="w-full sm:w-[50%]"><InputField label={'Your Name'} bind:value={formData.freelancerName} placeholder="John Doe"/></div>
 		<div in:fade={{ duration: 400, delay: 100 }} out:slide={{ y: -30, duration: 250 }} class="w-full sm:w-[50%]"><InputField type={'email'} label={'Your Email'} bind:value={formData.freelancerEmail} placeholder="john@doe.com"/></div></div>
 
 	{:else if active_step == 'ProjectInfo'}
@@ -87,10 +87,10 @@
 	{:else if active_step == 'Items'}
 	<h2 in:fade={{ duration: 400 }} out:slide={{ y: -30, duration: 250 }} class="sm:text-2xl text-xl">Proposal Items</h2>
 	{#each items as item, index}
-	<div class="list-item" in:fade={{ duration: 400 }} out:slide={{ y: -30, duration: 250 }}>
+	<div class="list-item gap-y-4 flex-col md:flex-row" in:fade={{ duration: 400 }} out:slide={{ y: -30, duration: 250 }}>
 		<input type="text" placeholder="Item Name" bind:value={item.title} class="bg-transparent placeholder:text-slate-500 text-slate-50 outline-none text-lg grow min-w-[65%]" />
 		<input type="number" placeholder="Price" bind:value={item.price} class="rounded-lg bg-slate-700/50 px-3 py-3 grow max-w-[200px] text-slate-50 placeholder:text-slate-500 outline-none" />
-		<button on:click={() => removeItem(index)} class=" pl-3 grow flex items-center justify-center text-slate-400 hover:text-slate-100 transition-all">
+		<button on:click={() => removeItem(index)} class=" absolute md:top-0 md:right-0 top-4 right-4 md:relative pl-3 grow flex items-center justify-center text-slate-400 hover:text-slate-100 transition-all">
 			<svg class="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
 				<path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
 			  </svg>
@@ -105,7 +105,7 @@
 		</div>
 	{/if}
 
-	<div class="mb-4 flex flex-col gap-y-4 sm:flex-row justify-between items-center mt-8" in:fade={{ duration: 400, delay: 100 }} out:slide={{ y: -30, duration: 250 }}><button on:click={addItem} class="small-button w-full sm:w-auto"> <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
+	<div class="mb-4 flex flex-col gap-y-4 sm:flex-row justify-between items-center mt-8" in:fade={{ duration: 400, delay: 100 }} out:slide={{ y: -30, duration: 250 }}><button on:click={addItem} class="small-button w-full sm:w-auto"> <svg class="md:w-4 md:h-4 h-3 w-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
 		<path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 1v16M1 9h16"/>
 	  </svg> Add Item</button>
 	<div class="text-2xl  font-bold text-slate-50" in:fade={{ duration: 400, delay: 100 }} out:slide={{ y: -30, duration: 250 }}><span class=" text-xl font-normal text-slate-400">Total</span> ${totalSum}</div>	
@@ -116,10 +116,10 @@
 		
 	<div>
 		{#each milestones as milestone, index}
-			<div class="list-item" in:fade={{ duration: 400 }} out:slide={{ y: -30, duration: 250 }}>
+			<div class="list-item  gap-y-4 flex-col md:flex-row" in:fade={{ duration: 400 }} out:slide={{ y: -30, duration: 250 }}>
 				<input type="text" placeholder="Milestone Title" bind:value={milestone.title} class="bg-transparent placeholder:text-slate-500 text-slate-50 outline-none text-lg grow min-w-[65%]" />
 				<input type="date" placeholder="Delivery Date" bind:value={milestone.deliveryDate} on:input={updateDates} class="rounded-lg bg-slate-700/50 px-3 py-3 grow max-w-[200px] text-slate-50 placeholder:text-slate-500 outline-none cursor-pointer" />
-				<button  on:click={() => removeMilestone(index)} class=" pl-3 grow flex items-center justify-center text-slate-400 hover:text-slate-100 transition-all" ><svg class="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+				<button  on:click={() => removeMilestone(index)} class=" md:top-0 md:right-0 absolute top-4 right-4 md:relative pl-3 grow flex items-center justify-center text-slate-400 hover:text-slate-100 transition-all" ><svg class="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
 					<path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
 				  </svg></button>
 			</div>
@@ -132,7 +132,7 @@
 	{/if}
 
 		<div class="mb-4 flex flex-col gap-y-4 sm:flex-row justify-between items-center mt-8" in:fade={{ duration: 400, delay: 100 }} out:slide={{ y: -30, duration: 250 }}>
-			<button on:click={addMilestone} class="small-button w-full sm:w-auto"> <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
+			<button on:click={addMilestone} class="small-button w-full sm:w-auto"> <svg class="md:w-4 md:h-4 h-3 w-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
 				<path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 1v16M1 9h16"/>
 			  </svg>Add Milestone</button>
 			  {#if milestones.length > 0 && (startDate || endDate)}
@@ -190,7 +190,7 @@
 	}
 
 	.list-item {
-		@apply bg-slate-800/50 border border-slate-600/20  shadow-xl shadow-black/10 rounded-lg p-4 flex justify-between mb-5 transition-all ; 
+		@apply relative bg-slate-800/50 border border-slate-600/20  shadow-xl shadow-black/10 rounded-lg p-4  flex justify-between mb-5 transition-all; 
 	}
 
 	.list-item:hover {
