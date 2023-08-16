4ca69d9d97baf52e1c55bc8af70a02eaa8b8f840
Fekry Studio <mfaiad2000@gmail.com>
2023-08-16 14:20:37 +0300

    typography cleanup

--- src/components/inputField.svelte
+++ src/components/inputField.svelte
@@ -24,7 +24,7 @@ onMount(() => {
 
 <p class="form-label">
 	{#if label}
-		<label class="label flex items-center  text-sm sm:text-base dark:text-slate-400 text-slate-500" for>{label}  {#if tooltip}
+		<label class="label flex items-center  text-sm sm:text-base dark:text-slate-500 text-slate-500" for>{label}  {#if tooltip}
 			<span class="tooltip-container">
 				<span class="ml-2 absolute top-[-10px]"><svg class="w-4 h-4 text-slate-500 hover:text-slate-300 transition-all " aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
 					<path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 9h2v5m-2 0h4M9.408 5.5h.01M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"/>
@@ -36,14 +36,14 @@ onMount(() => {
 	{/if}
 	{#if isTextarea}
 		<textarea 
-		class="input sm:text-lg text-base shadow-md shadow-transparent outline-none focus:shadow-slate-200 dark:focus:bg-slate-800/40 dark:focus:shadow-slate-950 focus:bg-slate-50  dark:focus:border-slate-700/50 focus:border-slate-200 hover:bg-slate-100/60 dark:hover:bg-slate-800/40 dark:bg-slate-800/70 bg-slate-100 dark:text-white text-slate-950 dark:border-slate-700/30 border-slate-200/80" 
+		class="input  text-base shadow-md shadow-transparent outline-none focus:shadow-slate-200 dark:focus:bg-slate-800/40 dark:focus:shadow-slate-950 focus:bg-slate-50  dark:focus:border-slate-700/50 focus:border-slate-200 hover:bg-slate-100/60 dark:hover:bg-slate-800/40 dark:bg-slate-800/70 bg-slate-100 dark:text-white text-slate-950 dark:border-slate-700/30 border-slate-200/80" 
 		bind:value={value} 
 		on:input={resizeTextarea}
 		rows="4" 
 		placeholder={placeholder}
 		></textarea>
 	{:else}
-		<input use:typeAction class="input sm:text-lg text-base shadow-md shadow-transparent outline-none focus:shadow-slate-200 dark:focus:bg-slate-800/40 dark:focus:shadow-slate-950 focus:bg-slate-50 focus:border-slate-200  dark:focus:border-slate-700/50 hover:bg-slate-100/60 dark:hover:bg-slate-800/40 dark:bg-slate-800/70 bg-slate-100 dark:text-white text-slate-950 dark:border-slate-700/30 border-slate-200/80" bind:value={value} placeholder={placeholder}/>
+		<input use:typeAction class="input  text-base shadow-md shadow-transparent outline-none focus:shadow-slate-200 dark:focus:bg-slate-800/40 dark:focus:shadow-slate-950 focus:bg-slate-50 focus:border-slate-200  dark:focus:border-slate-700/50 hover:bg-slate-100/60 dark:hover:bg-slate-800/40 dark:bg-slate-800/70 bg-slate-100 dark:text-white text-slate-950 dark:border-slate-700/30 border-slate-200/80" bind:value={value} placeholder={placeholder}/>
 	{/if}
 </p>
 
