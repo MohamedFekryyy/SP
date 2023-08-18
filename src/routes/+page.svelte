2eab0a653c6a12da2490d2b473980ba89f83f5e7
Fekry Studio <mfaiad2000@gmail.com>
2023-08-18 13:49:07 +0300

    removed supabase i think

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,5 @@
 <script>
     import Form from '../components/Form.svelte';
-    import { supabase } from '../supabaseClient';
     import ProgressBar from '../components/ProgressBar.svelte';
     import Modal from '../components/Modal.svelte';
     import { writable } from 'svelte/store';
@@ -17,62 +16,13 @@
     let totalSum;
 
 
-    async function addFormSubmission(formData, items, milestones, startDate, endDate, totalSum) {
-  // Inserting into the form_submissions table
-  const { data: submission, error: submissionError } = await supabase
-    .from('form_submissions')
-    .insert([
-      { 
-        clientName: formData.clientName,
-        clientCompany: formData.clientCompany,
-        clientEmail: formData.clientEmail,
-        freelancerEmail: formData.freelancerEmail,
-        freelancerName: formData.freelancerName,
-        projName: formData.projName,
-        projDescription: formData.projDescription,
-        projGoals: formData.projGoals,
-        preFilledText: formData.preFilledText,
-        start_date: startDate, 
-        end_date: endDate,
-        total_price: totalSum 
-      },
-    ]);
-
-  if (submissionError) {
-    console.error('Error adding submission:', submissionError);
-    return;
-  }
-
-  const submissionId = submission[0].id;
-
-  // Inserting into the proposal_items table
-  const proposalItems = items.map(item => ({
-    title: item.title,
-    price: item.price,
-    form_submission_id: submissionId
-  }));
-
-  await supabase.from('proposal_items').insert(proposalItems);
-
-  // Inserting into the milestones table
-  const milestoneItems = milestones.map(milestone => ({
-    title: milestone.title,
-    delivery_date: milestone.deliveryDate,
-    form_submission_id: submissionId
-  }));
-
-  await supabase.from('milestones').insert(milestoneItems);
-}
-
+   
 const isLoading = writable(false); //  state for PDF generation
 
   
 async function generatePDF() {
   try {
     isLoading.set(true); // Set the loading state to true
-
-    // Make sure the submission is added
-    await addFormSubmission(formData, items, milestones, startDate, endDate, totalSum);
 
     // Create an object with the data to send to the server
     const data = {
--- src/supabaseClient.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = 'https://tsptyfgxkrfiujltwwtg.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRzcHR5Zmd4a3JmaXVqbHR3d3RnIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTE2MDI4MjAsImV4cCI6MjAwNzE3ODgyMH0.A57TgbZxHYrYzNbAP228Y0M0bzO7_rlEbXfuA-bh6cU';
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
